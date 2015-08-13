48106edb87d5f7833040d08e2eaa2f3b40ee7b62
WhiteFox <son.nguyenhong1990@gmail.com>
2015-08-13 15:46:22 +0700

    Revert "Revert "TEST KHOI TAO DATA""
    This reverts commit dd728a2f16dc2223ab4f5f22639df2e68a603212.

---
+++ Assets/Scripts/Database.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Database : MonoBehaviour {
+
+	/*
+	 * //Lưu trữ thông tin
+		PlayerPrefs.SetString("username", "STGame Guys");
+		PlayerPrefs.Save();		
+	 * //Truy xuất thông tin
+		string value = PlayerPrefs.GetString("username");		
+	 * DeleteAll   //Xóa tất cả các dữ liệu tương ứng với game của bạn.
+	 * DeleteKey   //Xóa key và dữ liệu liên quan
+	 * GetFloat    //Trả về giá trị float tương ứng nếu nó tồn tại.
+	 * GetInt      //Trả về giá trị int tương ứng nếu nó tồn tại.
+	 * GetString   //Trả về giá trị string tương ứng nếu nó tồn tại.
+	 * HasKey      //Trả về true nếu key có tồn tại .
+	 * Save        //Lưu trữ tất cả dữ liệu được chỉnh sữa xuống đĩa.
+	 * SetFloat    //Lưu giá trị float theo key vào bố nhớ chính.
+	 * SetInt      //Lưu giá trị int theo key vào bố nhớ chính.
+	 * SetString   //Lưu giá trị string theo key vào bố nhớ chính.
+*/
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
